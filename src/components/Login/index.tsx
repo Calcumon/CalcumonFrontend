d9f2ea8001a09c453c33fa40503ca161c41a6cfb
Joey Gaitan <joeygaitan707@gmail.com>
2019-10-17 19:48:43 -0700

    added in banner

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import {Link} from 'react-router-dom'
 
+
 import { setAuthentication } from '../../actions/authentication'
 
 // TODO: What will be in our LoginInterface?
--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { bool } from 'prop-types';
+import { booleanLiteral } from '@babel/types';
 export interface Props {
     signUpAuthenication: Function;
 }
@@ -8,6 +10,7 @@ export interface State {
     email?: string;
     password: string;
     correctPassword: string;
+    error: boolean;
 }
  
 class Signup extends React.Component<Props, State> {
@@ -17,13 +20,56 @@ class Signup extends React.Component<Props, State> {
             username: 'name',
             email: "@gmail.com",
             password: 'password',
-            correctPassword: 'Same Password'
+            correctPassword: 'Same Password',
+            error: false
         };
     }
+
+    handleSignIn = () =>{
+        //#TODO update url to meet api standard 
+        fetch("/auth", {
+          method: "post",
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            username: this.state.username,
+            password: this.state.password
+          })
+        }).then((response) => response.json())
+        .then((data) => {
+            // TODO: find what data backend sends us
+            if (!data.status) {
+              this.setState({ error : true });
+            } else {
+              // TODO: Update redux to call authenticate action as true
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+            this.setState({ error : true });
+        })
+      
+      }
+
     render() { 
-        return ( <div>
-            <h1>hello</h1>
-        </div> );
+        return ( <div className="login">
+        <input type="string" 
+               name="username" 
+               id="inputEmail" 
+               placeholder="Username" 
+               onChange={(e) => this.setState({ username : e.target.value})}
+               required autoFocus />
+        <input type="password" 
+               name="inputPassword" 
+               id="inputPassword" 
+               className="form-control" 
+               placeholder="Password" 
+               onChange={(e) => this.setState({ password : e.target.value})}
+               required />
+        <button className="btn btn-lg btn-primary btn-block" type="submit" onSubmit={() => {
+          // TODO: validate data function
+          this.handleSignIn()
+        }}>Sign in</button>
+      </div> );
     }
 }
  
